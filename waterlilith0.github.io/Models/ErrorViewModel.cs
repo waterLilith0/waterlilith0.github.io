16a76c01b647275044a5f1461e46bdfe1fdf413e
Miriam Fix <fixm@landaumedia.de>
2025-05-14 10:26:54 +0200

    Add project files.

---
+++ waterlilith0.github.io/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace waterlilith0.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
