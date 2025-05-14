45618f9eb0fd2294308ffc76837a5431d080bb56
Miriam Fix <fixm@landaumedia.de>
2025-05-14 10:36:49 +0200

    remove api

--- waterlilith0.github.io/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace waterlilith0.github.io.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
