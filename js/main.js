50e657cc17e1ae384d6bd8651ab2f864333990ac
Miriam Fix <fixm@landaumedia.de>
2025-05-15 14:07:47 +0200

    Add dark mode toggle and update body styling
    - Updated `index.html` to include a dark mode toggle switch in the navigation.
    - Modified `<body>` class to "white" for background color change.
    - Implemented dark mode functionality in `main.js` with event listener for the toggle.
    - Added humorous comment insertion for testing/debugging purposes.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+const body = document.querySelector("body");
+
 const timeout = setInterval(() => {
   const elem = document.createComment("Crazy? I was crazy once! They put me in a room! A rubber room! A rubber room with rats! And Rats? Rats make me crazy!");
   document.body.appendChild(elem);
 }, 10);
 
+
+console.log("test");
+
+const darkModeToggle = document.querySelector("#darkModeToggle");
+darkModeToggle.addEventListener("click", () => {
+  darkMode();
+});
+
+const darkMode = (() => {
+  body.classList.toggle("black");
+});
+
+darkMode();
