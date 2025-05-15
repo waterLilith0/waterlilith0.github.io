7a4df639575c9f11a063ac82017230edad680195
Miriam Fix <fixm@landaumedia.de>
2025-05-15 13:12:52 +0200

    Enhance webpage structure and update Materialize files
    Updated `index.html` to improve layout with a navigation bar, welcome message, and footer. Removed commented-out text and added stylesheets and scripts for better design.
    
    In `main.js`, replaced the `crazy` variable with a new interval function that appends humorous comment nodes to the document body. Removed old console log statements and an infinite loop.
    
    Added `LICENSE` file specifying the MIT License for the project.
    
    Changes in `materialize.css` focus on modernizing styles and improving responsiveness.
    
    In `materialize.min.css`, introduced new color classes and styles for UI components, enhancing design and functionality.
    
    Updated `materialize.js` and `materialize.min.js` with new components like `Pushpin`, `FloatingActionButton`, and `Datepicker`, along with event handlers for improved user interaction. Added dropdown setup and animations for a dynamic interface.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-const crazy = document.children[1];
+const timeout = setInterval(() => {
+  const elem = document.createComment("Crazy? I was crazy once! They put me in a room! A rubber room! A rubber room with rats! And Rats? Rats make me crazy!");
+  document.body.appendChild(elem);
+}, 10);
 
-console.log(crazy);
-
-//while (true) {
-  
-//}
