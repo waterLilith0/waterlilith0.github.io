7ddaa21853d0d92ae19f76071164a80269e56138
Miriam Fix <fixm@landaumedia.de>
2025-05-16 11:36:21 +0200

    added basic gallery view

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const body = document.querySelector("body");
 
+M.AutoInit();
+
 const timeout = setInterval(() => {
   const elem = document.createComment("Crazy? I was crazy once! They put me in a room! A rubber room! A rubber room with rats! And Rats? Rats make me crazy!");
   document.body.appendChild(elem);
 }, 10);
 
-
-console.log("test");
 
 const darkModeToggle = document.querySelector("#darkModeToggle");
 darkModeToggle.addEventListener("click", () => {
