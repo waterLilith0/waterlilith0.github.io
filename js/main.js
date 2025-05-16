b348cb790fb47ad97925d25d9e01b934f2394e7c
Miriam Fix <fixm@landaumedia.de>
2025-05-16 11:44:37 +0200

    Add lazy loading for images and update JS initialization
    - Added `loading="lazy"` to `img` elements in `main.css` to improve page load performance.
    - Removed `M.AutoInit();` from `main.js`, affecting Materialize component initialization.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const body = document.querySelector("body");
 
-M.AutoInit();
-
 const timeout = setInterval(() => {
   const elem = document.createComment("Crazy? I was crazy once! They put me in a room! A rubber room! A rubber room with rats! And Rats? Rats make me crazy!");
   document.body.appendChild(elem);
